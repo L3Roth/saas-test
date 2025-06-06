e9932b15336ba7b82e1144f1a33d3a36442905d8
Alexander Noack <alexander.noack95@outlook.de>
2025-06-06 16:55:38 +0200

    add companion card, list and call to action area

--- saas-test/app/page.tsx
+++ saas-test/app/page.tsx
@@ -1,12 +1,48 @@
-import { Button } from '@/components/ui/button'
+import CTA from '@/components/cta'
+import CompanionCard from '@/components/companionCard'
+import CompanionsList from '@/components/companionsList'
 import React from 'react'
+import { recentSessions } from '../constants'
 
 const Page = () => {
   return (
-    <div>
-       <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>Lets get started</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard 
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivates & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard 
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
