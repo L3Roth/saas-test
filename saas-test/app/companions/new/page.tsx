61c277a1b7d8c5ab93eda2eddd4cd7c15adcd5a3
Alexander Noack <alexander.noack95@outlook.de>
2025-06-09 18:31:47 +0200

    add companion form, add several shadcn components

--- saas-test/app/companions/new/page.tsx
+++ saas-test/app/companions/new/page.tsx
@@ -1,6 +1,13 @@
+import CompanionForm from "@/components/companionForm"
+
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main className="min-lg_w-1/3 minmd:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm/>
+      </article>
+    </main>
   )
 }
 
